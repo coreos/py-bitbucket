f2b78e2e99aa091215201fd4555b36239eda8760
Joseph Schorr <joseph.schorr@coreos.com>
2015-04-28 18:00:12 -0400

    Add missing services accessor

--- bitbucket/repository.py
+++ bitbucket/repository.py
@@ -6,6 +6,7 @@ from urls import (repository_branches_url, repository_tags_url, repository_branc
 
 from deploykeys import BitBucketRepositoryDeployKeysClient
 from links import BitBucketRepositoryLinksClient
+from services import BitBucketRepositoryServicesClient
 
 class BitBucketRepositoryClient(object):
   """ Client class representing a repository in bitbucket. """
@@ -25,6 +26,12 @@ class BitBucketRepositoryClient(object):
   def repository_name(self):
     """ Returns the repository name. """
     return self._repository_name
+
+  def services(self):
+    """ Returns a resource for managing the services under this repository. """
+    return BitBucketRepositoryServicesClient(self._dispatcher, self._access_token,
+                                             self._access_token_secret, self._namespace,
+                                             self._repository_name)
 
   def links(self):
     """ Returns a resource for managing the links under this repository. """
